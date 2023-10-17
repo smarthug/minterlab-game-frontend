f2a106e0681323c665e5582bfb1241ed19adb4d1
smarthug <kirklayer@gmail.com>
2023-10-17 15:35:21 +0900

    nft check works

--- pages/key/[contractAddress]/[uuid].tsx
+++ pages/key/[contractAddress]/[uuid].tsx
@@ -1,10 +1,15 @@
 import { usePathname, useSearchParams } from "next/navigation";
+import { ethers } from "ethers";
+import contractFile from "./Contract.json";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+// const { data: walletClient, isError, isLoading } = useWalletClient()
 
 export default function UUID() {
   const pathname = usePathname();
   console.log(pathname);
   //   const pathArray = pathname.split("/");
-  const pathArray = pathname.match(/[^\/]+/g);
+  const pathArray = pathname?.match(/[^\/]+/g);
   console.log(pathArray);
   const contractAddress = pathArray?.[1];
   const uuid = pathArray?.[2];
@@ -18,10 +23,10 @@ export default function UUID() {
     const address = await signer.getAddress();
 
     console.log("BB");
-    let contractData = JSON.parse(JSON.stringify(contractFile));
+    // let contractData = JSON.parse(JSON.stringify(contractFile));
 
-    const NFTAddress = contractData.NFTAddress;
-    const NFTABI = contractData.NFTABI;
+    const NFTAddress = contractFile.NFTAddress;
+    const NFTABI = contractFile.NFTABI;
     const contract = new ethers.Contract(NFTAddress, NFTABI, provider);
     const contractWithSigner = contract.connect(signer);
     console.log(NFTAddress);
@@ -43,10 +48,12 @@ export default function UUID() {
       //     // var element = document.getElementById("p1");
       //     // element.innerHTML = "Invalid Token !!!";
       //   }
+      console.log(uuid);
     }
   }
   return (
     <div>
+      <ConnectButton />
       <h1>hi</h1>
       <button className="btn btn-primary w-64 rounded-full btn-outline">
         Button
@@ -57,12 +64,10 @@ export default function UUID() {
             <h1 className="text-3xl font-bold">Check NFT Game Key</h1>
             <div>contract Address : {contractAddress}</div>
             <div>uuid : {uuid}</div>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+            <p className="py-6">여기서</p>
+            <button className="btn btn-primary" onClick={checkNFTKey}>
+              Game key 검사하기
+            </button>
           </div>
         </div>
       </div>
